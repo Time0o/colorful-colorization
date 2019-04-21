98476610e2fe6fc06b63ee0328fbfa02965312bf
Timo Nicolai <timonicolai@arcor.de>
2019-04-21 09:31:49 +0000

    Simplify CrossEntropyLoss2d

--- colorization/modules/cross_entropy_loss_2d.py
+++ colorization/modules/cross_entropy_loss_2d.py
@@ -8,19 +8,6 @@ class CrossEntropyLoss2d(nn.Module):
         self.loss = nn.CrossEntropyLoss()
 
     def forward(self, outputs, labels):
-        outputs_flat = self._flatten(outputs)
-        labels_flat = self._flatten(labels)
+        labels_collapsed = labels.max(dim=1)[1]
 
-        labels_collapsed = labels_flat.max(dim=1)[1]
-
-        return self.loss(outputs_flat, labels_collapsed)
-
-    @staticmethod
-    def _flatten(batch):
-        n, h, w = batch.shape[:-1]
-
-        batch_reordered = batch.permute(0, 2, 3, 1)
-
-        batch_flat = batch_reordered.contiguous().view(n * h * w, -1)
-
-        return batch_flat
+        return self.loss(outputs, labels_collapsed)
