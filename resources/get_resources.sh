e4f2893fee3afaa1fdec7267e9bfacb21ac69e20
Timo Nicolai <timonicolai@arcor.de>
2019-04-22 09:15:42 +0000

    Add get_resources.sh

---
+++ resources/get_resources.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# ab gamut
+wget https://github.com/richzhang/colorization/raw/master/resources/pts_in_hull.npy \
+    -O ./resources/ab-gamut.npy
+
+# release models
+wget https://raw.githubusercontent.com/richzhang/colorization/master/models/colorization_deploy_v1.prototxt \
+    -O ./resources/colorization_deploy_v1.prototxt
+wget https://raw.githubusercontent.com/richzhang/colorization/master/models/colorization_deploy_v2.prototxt \
+    -O ./resources/colorization_deploy_v2.prototxt
+
+wget http://eecs.berkeley.edu/~rich.zhang/projects/2016_colorization/files/demo_v2/colorization_release_v2.caffemodel \
+    -O ./resources/colorization_release_v2.caffemodel
+wget http://eecs.berkeley.edu/~rich.zhang/projects/2016_colorization/files/demo_v2/colorization_release_v2_norebal.caffemodel \
+    -O ./resources/colorization_release_v2_norebal.caffemodel
+wget http://eecs.berkeley.edu/~rich.zhang/projects/2016_colorization/files/demo_v1/colorization_release_v1.caffemodel \
+    -O ./resources/colorization_release_v1.caffemodel
