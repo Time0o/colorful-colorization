c968ca16d6f0a73ceca7b4a15832403e2d83d7a4
Timo Nicolai <timonicolai@arcor.de>
2019-04-25 15:17:41 +0000

    Add --random-seed parameter to run_training.py

--- run_training.py
+++ run_training.py
@@ -2,6 +2,8 @@
 
 import argparse
 
+import torch
+
 import colorization.config as config
 from colorization.util.argparse import nice_help_formatter
 
@@ -9,7 +11,8 @@ from colorization.util.argparse import nice_help_formatter
 USAGE = \
 """run_training.py [-h|--help]
                        --config CONFIG
-                       [--default-config CONFIG]"""
+                       [--default-config CONFIG]
+                       [--random-seed SEED]"""
 
 
 if __name__ == '__main__':
@@ -25,7 +28,16 @@ if __name__ == '__main__':
                         metavar='CONFIG',
                         help="training default configuration JSON file")
 
+    parser.add_argument('--random-seed',
+                        metavar='SEED',
+                        type=int,
+                        help="torch random seed")
+
     args = parser.parse_args()
+
+    # set random seed
+    if args.random_seed is not None:
+        torch.manual_seed(args.random_seed)
 
     # load configuration file(s)
     cfg = config.get_config(args.config, args.default_config)
