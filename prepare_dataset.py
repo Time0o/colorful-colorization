2baaf720dac19147a245822a3ebcc012e621730e
Timo Nicolai <timonicolai@arcor.de>
2019-05-02 07:36:28 +0000

    Refactor prepare_dataset.py

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import os
+import random
 import sys
 from argparse import ArgumentParser
 from glob import glob
-from random import shuffle
 from shutil import move
 from subprocess import CalledProcessError, check_call
 
@@ -40,6 +40,74 @@ def _purge_toplevel(data_dir, file_ext):
     for f in os.listdir(datadir):
         if not f.endswith('.' + file_ext):
             os.remove(os.path.join(root, f))
+
+
+def _split_dataset(data_dir, file_ext, shuffle, val_split, test_split):
+    data_all = glob(os.path.join(data_dir, '*.' + file_ext))
+
+    if shuffle:
+        random.shuffle(data_all)
+
+    num_val = int(val_split * len(data_all))
+    num_test = int(test_split * len(data_all))
+    num_train = len(data_all) - num_val - num_test
+
+    data_train = data_all[:num_train]
+    data_val = data_all[num_train:(num_train + num_val)]
+    data_test = data_all[(num_train + num_val):]
+
+    for files, subdir in (data_train, 'train'), \
+                         (data_val, 'val'), \
+                         (data_test, 'test'):
+
+        path = os.path.join(data_dir, subdir)
+
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+        for f in files:
+            move(f, path)
+
+
+def _create_lmdbs(data_dir, convert_imageset):
+    if args.convert_imageset is None:
+        print("missing convert_imageset script location", file=sys.stderr)
+        sys.exit(1)
+
+    # create lmdb subdirectory
+    path = os.path.join(data_dir, 'lmdb')
+
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    # create one lmdb per subdirectory
+    for subdir in 'train', 'val', 'test':
+        # create dummy label text file
+        dummy_labels = subdir + '.txt'
+
+        with open(dummy_labels, 'w') as f:
+            f.write('\n'.join([
+                '/{} 0'.format(os.path.basename(f))
+                for f in os.listdir(os.path.join(data_dir, subdir))
+            ]))
+
+        # call conversion script
+        lmdb_subdir = os.path.join(data_dir, 'lmdb', subdir)
+
+        lmdb_args = [
+            args.convert_imageset,
+            os.path.abspath(os.path.join(data_dir, subdir)),
+            dummy_labels,
+            lmdb_subdir
+        ]
+
+        try:
+            check_call(lmdb_args)
+        except CalledProcessError:
+            print("failed to call lmdb creation script", file=sys.stderr)
+
+        # remove dummy label text file again
+        os.remove(dummy_labels)
 
 
 if __name__ == '__main__':
@@ -99,69 +167,26 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    if args.resize_height is not None or args.resize_width is not None:
+        raise ValueError('TODO')
+
+    # flatten and purge
     if args.flatten:
         _flatten_data_dir(args.data_dir, args.purge, args.file_ext)
     elif args.purge:
         _purge_toplevel(args.data_dir, args.file_ext)
 
-    data_all = glob(os.path.join(args.data_dir, '*.' + args.file_ext))
+    # split
+    _split_dataset(args.data_dir,
+                   args.file_ext,
+                   not args.no_shuffle,
+                   args.val_split,
+                   args.test_split)
 
-    if not args.no_shuffle:
-        shuffle(data_all)
-
-    num_val = int(args.val_split * len(data_all))
-    num_test = int(args.test_split * len(data_all))
-    num_train = len(data_all) - num_val - num_test
-
-    data_train = data_all[:num_train]
-    data_val = data_all[num_train:(num_train + num_val)]
-    data_test = data_all[(num_train + num_val):]
-
-    for subdir in 'train', 'val', 'test':
-        path = os.path.join(args.data_dir, subdir)
-
-        if not os.path.exists(path):
-            os.mkdir(path)
-
+    # create lmdbs
     if args.create_lmdb:
         if args.convert_imageset is None:
             print("missing convert_imageset script location", file=sys.stderr)
             sys.exit(1)
 
-        path = os.path.join(args.data_dir, 'lmdb')
-
-        if not os.path.exists(path):
-            os.mkdir(path)
-
-    for data_files, subdir in (data_train, 'train'), \
-                              (data_val, 'val'), \
-                              (data_test, 'test'):
-
-        subdir_ = os.path.join(args.data_dir, subdir)
-        for f in data_files:
-            move(f, subdir_)
-
-        subdir_labels = subdir_ + '.txt'
-        with open(subdir_labels, 'w') as f:
-            f.write('\n'.join([
-                '/{} 0'.format(os.path.basename(f))
-                for f in data_files
-            ]))
-
-        if args.create_lmdb:
-            lmdb_subdir = os.path.join(args.data_dir, 'lmdb', subdir)
-
-            lmdb_args = [args.convert_imageset]
-
-            if args.resize_height is not None:
-                lmdb_args += ['--resize_height', str(args.resize_height)]
-
-            if args.resize_width is not None:
-                lmdb_args += ['--resize_width', str(args.resize_width)]
-
-            lmdb_args += [os.path.abspath(subdir_), subdir_labels, lmdb_subdir]
-
-            try:
-                check_call(lmdb_args)
-            except CalledProcessError: 
-                print("failed to call lmdb creation script", file=sys.stderr)
+        _create_lmdbs(args.data_dir, args.convert_imageset)
