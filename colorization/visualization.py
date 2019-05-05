2d8b1b0abb29e2cd052c21f462858329bcf31c28
Timo Nicolai <timonicolai@arcor.de>
2019-05-05 23:58:33 +0200

    Implement AMT demo

--- colorization/visualization.py
+++ colorization/visualization.py
@@ -1,5 +1,8 @@
+import os
 import re
+from functools import partial
 from itertools import chain
+from operator import itemgetter
 
 import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
@@ -7,32 +10,72 @@ import numpy as np
 from matplotlib.lines import Line2D
 from matplotlib.transforms import blended_transform_factory
 
-from .util.image import rgb_to_lab
+from .util.image import Image, rgb_to_lab
 
 
 _FIGURE_WIDTH = 12
 _FIGURE_SPACING = 0.05
 
 
-def _subplots(r, c, figsize=None):
+def _subplots(r, c, use_gridspec=False):
     display_width = (_FIGURE_WIDTH - (c - 1) * _FIGURE_SPACING) / c
     figure_height = r * display_width + (r - 1) * _FIGURE_SPACING
 
-    fig = plt.figure(figsize=(_FIGURE_WIDTH, figure_height))
+    if use_gridspec:
+        fig = plt.figure(figsize=(_FIGURE_WIDTH, figure_height))
 
-    gs = gridspec.GridSpec(r, c)
-    gs.update(wspace=_FIGURE_SPACING, hspace=_FIGURE_SPACING)
+        gs = gridspec.GridSpec(r, c)
+        gs.update(wspace=_FIGURE_SPACING, hspace=_FIGURE_SPACING)
 
-    axes = np.empty((r, c), dtype=np.object)
+        axes = np.empty((r, c), dtype=np.object)
 
-    for r_ in range(r):
-        for c_ in range(c):
-            axes[r_, c_] = plt.subplot(gs[r_ * c + c_])
+        for r_ in range(r):
+            for c_ in range(c):
+                axes[r_, c_] = plt.subplot(gs[r_ * c + c_])
+    else:
+        fig, axes = plt.subplots(r, c, figsize=(_FIGURE_WIDTH, figure_height))
+        axes = axes.reshape(r, c)
 
     for ax in axes.flatten():
-        ax.axis('off')
+        ax.tick_params(
+            axis='both',
+            which='both',
+            bottom=False,
+            top=False,
+            left=False,
+            right=False,
+            labelbottom=False,
+            labeltop=False,
+            labelleft=False,
+            labelright=False)
 
     return fig, axes
+
+
+def _bbox(fig, ax):
+    r = fig.canvas.get_renderer()
+
+    return ax.get_tightbbox(r).transformed(fig.transFigure.inverted())
+
+
+def _subplot_divider(fig, axes, orientation, n):
+    bbox = partial(_bbox, fig)
+
+    line2d = partial(Line2D,
+                     transform=fig.transFigure,
+                     color='k',
+                     linestyle='--')
+
+    if orientation == 'horizontal':
+        y = (bbox(axes[n, 0]).y1 + bbox(axes[n + 1, 0]).y0) / 2
+        line = line2d([0, 1], [y, y])
+    elif orientation == 'vertical':
+        x = (bbox(axes[0, n]).x1 + bbox(axes[0, n + 1]).x0) / 2
+        line = line2d([x, x], [0, 1])
+    else:
+        raise ValueError("orientation must be 'horizontal' or 'vertical'")
+
+    fig.add_artist(line)
 
 
 def learning_curve_from_log(filename,
@@ -85,7 +128,7 @@ def annealed_mean_demo(model, image_set, ts=None, verbose=False):
 
     assert len(ts) % 2 == 1
 
-    _, axes = _subplots(len(image_set), len(ts))
+    _, axes = _subplots(len(image_set), len(ts), use_gridspec=True)
 
     for c, t in enumerate(ts):
         if verbose:
@@ -124,7 +167,8 @@ def good_vs_bad_demo(model_norebal,
     assert len(image_set_good) % 2 == 1
     assert len(image_set_bad) % 2 == 1
 
-    fig, axes = _subplots(len(image_set_good) + len(image_set_bad), 4)
+    fig, axes = _subplots(
+        len(image_set_good) + len(image_set_bad), 4, use_gridspec=False)
 
     for r, image in enumerate(chain(image_set_good, image_set_bad)):
         if verbose:
@@ -143,19 +187,76 @@ def good_vs_bad_demo(model_norebal,
         axes[r, 3].imshow(image.get())
 
     # plot divider
-    trans = blended_transform_factory(
-        fig.transFigure, axes[len(image_set_good) - 1, 0].transAxes)
+    plt.tight_layout()
 
-    line = Line2D([0.1, 0.925],
-                  [-0.025, -0.025],
-                  color='k',
-                  linestyle='--',
-                  transform=trans)
-
-    fig.lines.append(line)
+    _subplot_divider(fig, axes, 'horizontal', len(image_set_good))
 
     # add titles
     axes[0, 0].set_title("Input")
     axes[0, 1].set_title("Classification")
     axes[0, 2].set_title("Classification\n(w/ Rebalancing)")
     axes[0, 3].set_title("Ground Truth")
+
+
+def amt_results_demo(model,
+                     results_dir,
+                     rows=4,
+                     columns_best=3,
+                     columns_worst=1,
+                     verbose=False):
+
+    # parse results
+    results = {}
+
+    with open(os.path.join(results_dir, 'results.txt'), 'r') as f:
+        for line in f:
+            path, result = line.split()
+
+            results[Image.load(os.path.join(results_dir, path))] = float(result)
+
+    assert len(results) >= rows * (columns_worst + columns_best)
+
+    images_sorted = [
+        img for img, _ in sorted(results.items(), key=itemgetter(1))
+    ]
+
+    # plot results
+    fig, axes = _subplots(rows, (columns_best + columns_worst) * 2)
+
+    images_show = list(reversed(
+        images_sorted[:(rows * columns_worst)] + \
+        images_sorted[-(rows * columns_best):]
+    ))
+
+    for c in range(columns_best + columns_worst):
+        for r in range(rows):
+            n = c * rows + r
+
+            if verbose:
+                print("running prediction for image {}".format(n + 1))
+
+            image = images_show[n]
+
+            axes[r, 2 * c].imshow(image.get())
+            axes[r, 2 * c + 1].imshow(image.predict(model).get())
+
+            axes[r, 2 * c].set_ylabel("{}%".format(int(100 * results[image])))
+
+    # plot divider
+    plt.tight_layout()
+
+    _subplot_divider(fig, axes, 'vertical', 2 * columns_best - 1)
+
+    # add titles
+    for i, ax in enumerate(axes[0, :]):
+        if i % 2 == 0:
+            ax.set_title("Ground Truth")
+        else:
+            ax.set_title("Ours")
+
+    fmt = '{}' + ' ' * 140 + '{}'
+
+    suptitle = fmt.format(r'Fooled more often $\longleftarrow$',
+                          r'$\longrightarrow$ Fooled less often')
+
+    fig.suptitle(suptitle, y=0)
