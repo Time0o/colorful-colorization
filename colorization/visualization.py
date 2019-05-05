96f8c468bb419d366f3271795b75512258f90fd3
Timo Nicolai <timonicolai@arcor.de>
2019-05-05 17:45:57 +0200

    Remove learning curve extrapolation

--- colorization/visualization.py
+++ colorization/visualization.py
@@ -9,8 +9,6 @@ from scipy.stats import linregress
 def learning_curve_from_log(filename,
                             loss_regex,
                             smoothing_alpha=0.05,
-                            extrapolate_epsilon=0.05,
-                            extrapolate=0,
                             ax=None):
 
     if ax is None:
@@ -43,31 +41,6 @@ def learning_curve_from_log(filename,
     # plot loss curves
     p = ax.plot(iterations, losses, alpha=.5)
     ax.plot(iterations, losses_smoothed, color=p[0].get_color())
-
-    # extrapolate loss developement
-    if extrapolate > iterations[-1]:
-        # find straight line segment
-        rdp_ = rdp(np.stack((iterations, losses_smoothed)).T,
-                   epsilon=extrapolate_epsilon)
-
-        lin_beg, lin_end = np.hsplit(rdp_, 2)[0][[-2, -1]]
-
-        for i in lin_beg, lin_end:
-            ax.axvline(i, color=p[0].get_color(), alpha=.5, linestyle='--')
-
-        iterations_list = list(iterations)
-        i_beg = iterations_list.index(lin_beg)
-        i_end = iterations_list.index(lin_end)
-
-        # extrapolate straight line segment
-        m, b, _, _, _ = linregress(iterations[i_beg:(i_end + 1)],
-                                   losses_smoothed[i_beg:(i_end + 1)])
-
-        iterations_extrapolated = np.arange(1, extrapolate + 1)
-        losses_extrapolated = iterations_extrapolated * m + b
-
-        # plot extrapolation
-        plt.plot(iterations_extrapolated, losses_extrapolated, linestyle='--')
 
     # format plot
     ax.set_xlabel("Iteration")
