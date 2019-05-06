d393439ba2a4d38b557ea55f2e8a0cd4b5e79246
Timo Nicolai <timonicolai@arcor.de>
2019-05-06 23:33:19 +0200

    Make ImageDirectory auto-detect labels

--- colorization/data/image_directory.py
+++ colorization/data/image_directory.py
@@ -8,11 +8,15 @@ from ..util.image import imread
 
 
 class ImageDirectory(Dataset):
-    def __init__(self, root, transform=None):
+    LABEL_FILENAME = 'labels.txt'
+
+    def __init__(self, root, transform=None, label_transform=int):
         self.root = root
         self.transform = transform
+        self.label_transform = label_transform
 
         self._paths = []
+        self._labels = None
         self._get_paths()
 
     def __getitem__(self, index):
@@ -21,7 +25,10 @@ class ImageDirectory(Dataset):
         if self.transform:
             img = self.transform(img)
 
-        return img
+        if self._labels is not None:
+            return img, self._labels[index]
+        else:
+            return img
 
     def __len__(self):
         return len(self._paths)
@@ -39,6 +46,7 @@ class ImageDirectory(Dataset):
         self._root = root
 
     def _get_paths(self):
+        # build list of image paths
         image_extensions = self._image_extensions()
 
         for path in glob(os.path.join(self.root, '*')):
@@ -46,6 +54,26 @@ class ImageDirectory(Dataset):
 
             if any([ext == ext_ for ext_ in image_extensions]):
                 self._paths.append(path)
+
+        # check whether label file exists
+        labels_path = os.path.join(self._root, self.LABEL_FILENAME)
+
+        if not os.path.exists(labels_path):
+            return
+
+        # if so, build list of labels
+        label_dict = {}
+
+        with open(labels_path, 'r') as f:
+            for line in f:
+                path, val = line.split()
+
+                path = os.path.join(self._root, path)
+                val = self.label_transform(val)
+
+                label_dict[path] = val
+
+        self._labels = [label_dict[path] for path in self._paths]
 
     @staticmethod
     def _image_extensions():
