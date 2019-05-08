f985bfa6b8adc3d1de8e5912ee4bc7fd141e9403
Timo Nicolai <timonicolai@arcor.de>
2019-05-08 21:15:26 +0000

    Update get_ctest.sh

--- data/get_ctest.sh
+++ data/get_ctest.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 DIR=./data
 
-CTEST10K_DIR="$DIR/ctest10k"
-CTEST10K_TXT="$CTEST10K_DIR/ctest10k.txt"
-
+CVAL48K_DIR="$DIR/cval48k"
+CVAL10K_DIR="$DIR/cval10k"
 CVAL1K_DIR="$DIR/cval1k"
-CVAL1K_TXT="$CVAL1K_DIR/cval1k.txt"
+
+LABELFILE="labels.txt"
 
 get_file() {
     wget -q --show-progress "$1" -O "$2"
@@ -16,32 +16,41 @@ copy_images() {
     echo "copying images into $1..."
 
     while read -r line; do
-        cp "$imagenet_root/${line%% *}" "$1"
-    done < "$2"
+        cp "$imagenet_val_root/${line%% *}" "$1"
+    done < "$1/$LABELFILE"
 }
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $(basename "$0") IMAGENET_VAL_ROOT" 2>&1
+if [ $# -ne 2 ]; then
+    echo "Usage: $(basename "$0") IMAGENET_VAL_ROOT IMAGENET_VAL_LABELS" 2>&1
     exit 1
 fi
 
+imagenet_val_root="$1"
+imagenet_val_labels="$2"
+
+# create image directories
+mkdir -p "$CVAL48K_DIR" "$CVAL10K_DIR" "$CVAL1K_DIR"
+
 # get image lists
-mkdir -p "$CTEST10K_DIR" "$CVAL1K_DIR"
+paste -d " " <(find "$imagenet_val_root" -type f | sort | head -n 48000) \
+             <(cat "$imagenet_val_labels" | head -n 48000) \
+             > "$CVAL48K_DIR/$LABELFILE"
 
 get_file \
 "https://raw.githubusercontent.com/AruniRC/colorizer-fcn/master/lists/ctest10k.txt" \
-"$CTEST10K_TXT"
+"$CVAL10K_DIR/$LABELFILE"
 
 get_file \
 "https://raw.githubusercontent.com/AruniRC/colorizer-fcn/master/lists/cval1k.txt" \
-"$CVAL1K_TXT"
+"$CVAL1K_DIR/$LABELFILE"
 
-for txt in "$CTEST10K_TXT" "$CVAL1K_TXT"; do
-    sed -i 's/^\/.*\///g' "$txt"
+for dir in "$CVAL48K_DIR" "$CVAL10K_DIR" "$CVAL1K_DIR"; do
+    labelfile="$dir/$LABELFILE"
+    sed -i 's/^\/.*\///g' "$labelfile"
+    sort "$labelfile" -o "$labelfile"
 done
 
 # copy images
-imagenet_root="$1"
-
-copy_images "$CTEST10K_DIR" "$CTEST10K_TXT"
-copy_images "$CVAL1K_DIR" "$CVAL1K_TXT"
+copy_images "$CVAL48K_DIR"
+copy_images "$CVAL10K_DIR"
+copy_images "$CVAL1K_DIR"
